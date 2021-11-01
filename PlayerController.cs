7f855792d3e487979506817fcb7b4ba4d1862494
Farhan Yuko Pratama <farhanyukopratama@student.ittelkom-sby.ac.id>
2021-11-01 19:37:09 +0700

    Change hello world menjadi mangga dan add apel

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,7 +7,10 @@ namespace HelloWorld
         public static void Main(string[].args)
         {
             int mangga, apel, hasil = 0;
-            Console.writeLine("Hello World");
+            Console.writeLine("Mangga = ");
+            mangga = int.Parse(Console.ReadLine());
+            Console.writeLine("Apple = ");
+            apel = int.Parse(Console.ReadLine());
         }
     }
 }
