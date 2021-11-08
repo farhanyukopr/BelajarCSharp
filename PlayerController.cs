eefc976192019cf2a8c1cfbbfffc5ebf771fa07b
Farhan Yuko Pratama <farhanyukopratama@student.ittelkom-sby.ac.id>
2021-11-08 13:59:42 +0700

    change mangga to jambu

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,9 +6,9 @@ namespace HelloWorld
     {
         public static void Main(string[].args)
         {
-            int mangga, apel, hasil = 0;
-            Console.writeLine("Mangga = ");
-            mangga = int.Parse(Console.ReadLine());
+            int jambu, apel, hasil = 0;
+            Console.writeLine("Jambu = ");
+            jambu = int.Parse(Console.ReadLine());
             Console.writeLine("Apple = ");
             apel = int.Parse(Console.ReadLine());
         }
